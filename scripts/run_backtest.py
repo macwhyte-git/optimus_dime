05c63db0117eb92646d69426ce38fbc7e59b28fd
Mac Whyte <macwhyte.inbox@gmail.com>
2026-07-18 21:55:38 +0100

    Added code to src/data/database.py to create a local sqlite db file for storing data. Updated run_backtest to make use of that file.

--- scripts/run_backtest.py
+++ scripts/run_backtest.py
@@ -1,3 +1,7 @@
+import pandas as pd
+from src.data.database import Database
+
+#Import trading parameters
 from config.trading_parameters import (
     SYMBOL,
     BACKTEST_START,
@@ -5,6 +9,7 @@ from config.trading_parameters import (
     TIMEFRAME
 )
 
+#Use alpaca.py from src/brokers to get historical prices and store them in a pandas dataframe
 from src.brokers.alpaca import get_historical_prices
 
 df = get_historical_prices(
@@ -14,4 +19,20 @@ df = get_historical_prices(
     timeframe=TIMEFRAME
 )
 
+#Get the high-level information about the dataframe and print the first 5 rows
+print(df.info())
 print(df.head())
+
+#Store the dataframe in a sqlite database using the Database class from src/data/database.py
+db = Database()
+
+df.to_sql(
+    "stock_bars", #table name
+    db.conn, #connection
+    if_exists="replace", #append or replace
+    index=True,
+    index_label=["symbol", "timestamp"]
+)
+
+db.commit()
+db.close()
